7af8983daa268115e8c3fbdae281d2f2d41abd2b
Hamish <Hamish@pageland.co.nz>
2020-02-01 08:45:31 +1300

    Day 1 End - movement

---
+++ Assets/LookAtDirection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtDirection : MonoBehaviour
+{
+    Vector3 prevPos = new Vector3();
+
+    public GameObject cam;
+    public float lookSpeed = 5;
+
+    private void Update()
+    {
+        Vector3 dir;
+
+        dir = transform.position - prevPos;
+
+        dir = dir.normalized;
+
+        //dir = transform.position + dir;
+        
+        if (dir.magnitude > 0.1f)
+        {
+            Vector3 up;
+
+            up = transform.position - cam.transform.position;
+
+
+            //Lerp to dir from current rotation
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir, up), lookSpeed * Time.deltaTime);
+
+            prevPos = transform.position;
+        }
+
+        
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float poleStop = 90;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+
+        Vector2 input = new Vector3(Input.GetAxis("Vertical"), -Input.GetAxis("Horizontal"));
+
+        input.y = input.y * (Mathf.Abs(Mathf.Sin(rotation.x / 360)) + 1);
+
+        Debug.Log(input.y);
+
+        rotation += new Vector3(input.x, input.y);
+        rotation.z = 0f;
+
+        //rotation.x = Mathf.Clamp(rotation.x, -80, 80);
+        //Debug.Log(rotation.x);
+
+        if (rotation.x > poleStop && rotation.x < 180)
+        {
+            rotation.x = poleStop;
+        }else if (rotation.x < 360 - poleStop && rotation.x > 180)
+        {
+            rotation.x = 360 - poleStop;
+        }
+
+        transform.rotation = Quaternion.Euler(rotation);
+
+    }
+
+}

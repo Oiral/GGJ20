0ac4094cfa0d0626308b5c268fdd8835c8ef541a
Hamish <Hamish@pageland.co.nz>
2020-02-01 12:48:45 +1300

    Added in some panels

---
+++ Assets/Panel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour
+{
+    public Node attachedNode;
+
+    public void Explode()
+    {
+        //Once it explodes
+        attachedNode.hasAttached = false;
+        attachedNode = null;
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.velocity = transform.up * 5;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Explode();
+        }
+    }
+}

ce4f155df06da635b950f7aec14b0055ed887ca0
Hamish <Hamish@pageland.co.nz>
2020-02-01 20:27:52 +1300

    Added in post and more picking up

--- Assets/PanelSpawner.cs
+++ Assets/PanelSpawner.cs
@@ -4,36 +4,5 @@ using UnityEngine;
 
 public class PanelSpawner : MonoBehaviour
 {
-    public float spawnTime = 5f;
 
-    float timer = 1f;
-
-    public GameObject panelPrefab;
-
-    private void Update()
-    {
-        if (timer > 0)
-        {
-            timer -= Time.deltaTime;
-            if (timer <= 0)
-            {
-                timer = 0;
-                SpawnPanel();
-            }
-        }
-    }
-
-    void SpawnPanel()
-    {
-        GameObject spawnedPanel = Instantiate(panelPrefab, transform.position, transform.rotation, null);
-        if (GetComponent<Panel>() != null)
-        {
-
-        }
-    }
-
-    public void CollectPanel()
-    {
-        timer = spawnTime;
-    }
 }

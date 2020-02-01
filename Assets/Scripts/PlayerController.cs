a259a58729d59e8036d9348257a0ece46ed9f42b
Hamish <Hamish@pageland.co.nz>
2020-02-02 10:45:36 +1300

    Animation controller working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,45 +22,10 @@ public class PlayerController : MonoBehaviour
     {
         Move();
 
-        if (Input.GetButtonDown("Interact" + player))
-        {
-            Debug.Log("Repair test");
-            //Raycast down
-            RaycastHit hit;
-            // Does the ray intersect any objects excluding the player layer
-            if (Physics.Raycast(pickUpLocation.transform.position, pickUpLocation.transform.TransformDirection(Vector3.up), out hit, 1))
-            {
-                Debug.DrawLine(pickUpLocation.transform.position, hit.point, Color.green, 10f);
-                Debug.DrawRay(pickUpLocation.transform.position, pickUpLocation.transform.TransformDirection(Vector3.up), Color.green, 1);
-                Debug.Log(hit.transform.gameObject,gameObject);
-
-                if (carrying != null)
-                {
-                    //If we are carry
-                    //Check if we hit a need to be repaired panel
-                    if (hit.transform.GetComponent<Panel>() != null)
-                    {
-                        if (hit.transform.GetComponent<Panel>().destroyed)
-                        {
-                            GameManager.instance.RepairPanel(hit.transform.gameObject);
-                            carrying.GetComponent<PickUpPanel>().Place();
-                            carrying = null;
-                        }
-                    }
-                }
-                else
-                {
-                    //Check if we can pick something up
-                    if (hit.transform.GetComponentInParent<PickUpPanel>())
-                    {
-                        //Pick up the panel
-                        carrying = hit.transform.GetComponentInParent<PickUpPanel>().gameObject;
-                        carrying.GetComponent<PickUpPanel>().PickUp(pickUpLocation);
-                    }
-                }
-
-            }
-        }
+        Interaction();
+        
+        CheckForFall();
+        
     }
 
     void Move()
@@ -110,6 +75,55 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.rotation = Quaternion.Euler(rotation);
+
+    }
+
+    void Interaction()
+    {
+        if (Input.GetButtonDown("Interact" + player))
+        {
+            Debug.Log("Repair test");
+            //Raycast down
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(pickUpLocation.transform.position, pickUpLocation.transform.TransformDirection(Vector3.up), out hit, 1))
+            {
+                Debug.DrawLine(pickUpLocation.transform.position, hit.point, Color.green, 10f);
+                Debug.DrawRay(pickUpLocation.transform.position, pickUpLocation.transform.TransformDirection(Vector3.up), Color.green, 1);
+                Debug.Log(hit.transform.gameObject, gameObject);
+
+                if (carrying != null)
+                {
+                    //If we are carry
+                    //Check if we hit a need to be repaired panel
+                    if (hit.transform.GetComponent<Panel>() != null)
+                    {
+                        if (hit.transform.GetComponent<Panel>().destroyed)
+                        {
+                            GameManager.instance.RepairPanel(hit.transform.gameObject);
+                            carrying.GetComponent<PickUpPanel>().Place();
+                            carrying = null;
+                        }
+                    }
+                }
+                else
+                {
+                    //Check if we can pick something up
+                    if (hit.transform.GetComponentInParent<PickUpPanel>())
+                    {
+                        //Pick up the panel
+                        carrying = hit.transform.GetComponentInParent<PickUpPanel>().gameObject;
+                        carrying.GetComponent<PickUpPanel>().PickUp(pickUpLocation);
+                    }
+                }
+
+            }
+        }
+    }
+
+    void CheckForFall()
+    {
+        //Let do a box cast down
 
     }
 

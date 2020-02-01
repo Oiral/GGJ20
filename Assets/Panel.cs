b788b49a04b301cd24836e43f46bc44558011ba7
Hamish <Hamish@pageland.co.nz>
2020-02-02 12:05:17 +1300

    Auto stash before merge of "master" and "origin/master"

--- Assets/Panel.cs
+++ Assets/Panel.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Panel : MonoBehaviour
 {
     public bool destroyed = false;
 
+    [HideInInspector]
+    public VisualEffect destroyParticle;
 
+    private void Start()
+    {
+        destroyParticle = GetComponentInChildren<VisualEffect>();
+
+        destroyParticle.Stop();
+    }
 }

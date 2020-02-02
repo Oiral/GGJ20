76e1ef852fd73daab582e67a3e4067016796537b
Hamish <Hamish@pageland.co.nz>
2020-02-02 16:34:40 +1300

    Added in start screen

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    float timeElapsed;
+
+
+    private void Update()
+    {
+        if (timeElapsed > 0.5f)
+        {
+
+            if (Input.GetButtonDown("Interact0") || Input.GetButtonDown("Interact1"))
+            {
+                //Load the scene
+                SceneManager.LoadScene(1);
+                return;
+            }
+        }
+        else
+        {
+            timeElapsed += Time.deltaTime;
+        }
+    }
+}
